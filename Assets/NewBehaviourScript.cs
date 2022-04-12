114d28a9f2867ef61a212f0e90b1bfc4da81ea45
Alex Shaham <ajshaham@gmail.com>
2022-04-12 01:19:47 -0700

    Split1 Push
    Solver doesnt work.
    Cube and base functionality done.
    Next steps are to "wordleify" it.

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,48 @@
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Dictionary<int, string> hash = new Dictionary<int, string>();
+    // Dictionary<int, string> hashScrambled = new Dictionary<int, string>();
+    // char[][] words = new char[6][];
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     // Assign the words to the cube
+    //     /* ---NEED TO DO ---
+    //         Add word to each side of cube
+    //         Randomly generate the words
+    //         Scramble each word using the functions to shift the cubes around
+    //             Build these functions
+    //             Rig cube
+    //             Need coloring as well...
+    //     */
+    //     hash.Add(0, "FACE");
+    //     hash.Add(1, "ROLL");
+    //     hash.Add(2, "CHAD");
+    //     hash.Add(3, "COOL");
+    //     hash.Add(4, "THAT");
+    //     hash.Add(5, "HARD");
+
+    //     for (int i = 0; i < hash.Count; i++)
+    //     {
+    //         words[i] = new char[4] {hash[i][0], hash[i][1], hash[i][2], hash[i][3]};
+    //         UnityEngine.Debug.Log(words[i][0] + "" + words[i][1] + "" + words[i][2] + "" + words[i][3]);
+    //     }
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     // Print();
+    // }
+
+    // void Print()
+    // {
+    //     UnityEngine.Debug.Log(words[0][0] + words[0][1] + words[0][2] + words[0][3]);
+    // }
+}
